9a17929a15c5762d72316006b90733a0330ef414
GLRMorais <guilherme@getwayautomacao.com.br>
2018-11-06 13:46:47 -0200

    final da aula 3

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -13,18 +13,21 @@ for (var i = 0; i < pacientes.length; i++) {
 
     if (peso <= 0 || peso > 1000) {
         console.log("Peso inválido");
-        tdImc.textContent = "Peso inválido!";
+        tdimc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido")
         pesoEhValido = false;
     }
     if (altura <= 0 || altura >= 3) {
         console.log("Altura inválida");
-        tdImc.textContent = "Altura inválida!";
+        tdimc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido")
         alturaEhValida = false;    }
     if (pesoEhValido && alturaEhValida) {
         var imc = peso / (altura * altura);
         tdimc.textContent = imc.toFixed(2);
     } else {
         tdimc.textContent = "Altura e/ou peso inválidos!";
+        paciente.classList.add("paciente-invalido")
     }
 }
 
